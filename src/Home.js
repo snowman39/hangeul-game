5d5eb9bc73e70f0a05e788ad07d17de13d365364
Snowman <snowman@gimmingiui-MacBook-Pro.local>
2019-12-17 17:35:23 +0900

    태원이형 code 합치고, 방만들거나, 들어가면 해당 url로 이동하게 해둠

--- src/Home.js
+++ src/Home.js
@@ -24,103 +24,91 @@ export default function Home() {
         document.getElementsByClassName('enter-room')[0].style.display = "block";
         document.getElementsByClassName('new-room')[0].style.display = "block";
     }
-
-    
     const onLogin = (e) => {
         e.preventDefault();
         if (!name)  return alert('별명을 입력하세요.');
-
         firestore.collection('users').doc(name).get()
-        .then(function(doc){
-            if(doc.exists)  localStorage.setItem('uid', name);
+        .then((doc) => {
+            if(doc.exists) return alert('이미 있는 별명입니다.');
             else{
                 firestore.collection('users').doc(name).set({
                 user: name,
                 best_score: 0
                 })}
+                localStorage.setItem('uid', name);
                 setLogin(1);
-            })
-            .catch(function(error){
-                return alert(error);
-            })
+        })
+        .catch((error) => {
+            return alert(error);
+        })
     }
-
+    const onNew = (e) => {
+        e.preventDefault();
+        if(!code)  return alert('암호를 입력하세요.');
+        firestore.collection('rooms').doc(code).get()
+        .then((doc) => {
+            if(doc.exists)  return alert('이미 있는 방입니다.');
+            else  {
+                firestore.collection('rooms').doc(code).set(
+                    {
+                        users: [{user: localStorage.getItem('uid'), score_thisgame: 0}],
+                        how_many: 1
+                    },
+                    { merge: true },
+                )
+                localStorage.setItem('code', code);
+                window.location = `Room/${localStorage.getItem('code')}`
+            }
+        })
+        .catch((error) => {
+            return alert(error);
+        });
+    }
     const onEnter = (e) => {
         e.preventDefault();
         if (!code)  return alert('암호를 입력하세요.');
         let roomRef = firestore.collection('rooms').doc(code);
-        roomRef.get().then(function(doc) {
+        roomRef.get().then((doc) => {
             if(doc.exists) {
                 let users_local = doc.data().users;                             //users 목록을 users_local로 받아옴
                 let found = false;
-                users_local.forEach(function(user){
-                    if(user.user === localStorage.getItem('userName'))      found = true;
-                })                                                             //users_local 안에 이미 이 사람이 존재하는지 존재하지 않는지 확인(found로)
-        
+                users_local.forEach((user) => {
+                    if(user.user === localStorage.getItem('uid'))  found = true;
+                })                                                              //users_local 안에 이미 이 사람이 존재하는지 존재하지 않는지 확인(found로)
                 if(!found)                                                      //새로온 유저라면
                 {
-                users_local = users_local.concat([{user: localStorage.getItem('userName'), score_thisgame: 0}]);       //users를 새로 update
+                users_local = users_local.concat([{user: localStorage.getItem('uid'), score_thisgame: 0}]);       //users를 새로 update
                 roomRef.set(
                     {
                         users: users_local,
                         how_many: doc.data().how_many + 1
-                    }
-                        ).then(function() {
-                        })
-                        .catch(function(error){
-                            return alert("단어 입력에 실패했어요!" + error)
-                        });
-
-                    }
+                    })
+                    .then(() => {
+                        localStorage.setItem('code', code);
+                        window.location = `Room/${localStorage.getItem('code')}`
+                    })
+                    .catch((error) => {
+                        return alert("단어 입력에 실패했어요!" + error)
+                    });
                 }
+            }
             else {
-                return alert("Sorry, we don't have a room like that");
+                return alert("해당 암호에 맞는 방이 없습니다.");
             }
             console.log(`${code} 방에 입장하셨씁니다`);
             console.log(doc.data().users);
             console.log("how many is", doc.data().how_many);                            //그냥 콘솔 확인하려구 찍어놓음. 지울 예정
-
             })
             .catch(function(error){
                 return alert(error);
             })
-
-        
-    }
-
-    const onNew = (e) => {
-        e.preventDefault();
-        if(! code)  return alert('암호를 입력하세요.');
-        firestore.collection('rooms').doc(code).get().then(function(doc)
-        {
-            if(doc.exists)
-            {
-                return alert('Sorry, already have it');
-            }
-            else{
-                firestore.collection('rooms').doc(code).set(
-                    {
-                        users: [{user: localStorage.getItem('userName'), score_thisgame: 0}],
-                        how_many: 1
-                    },
-                    { merge: true }
-                )
-        
-            }
-        })
-        .catch(function(error){
-            return alert(error);
-        });
-
-
-
     }
     return (
         <div className="background">  
             <div>
             <div className ="rank">
                 <Link exact to="/Rank">
-                <img src = {sejong} className="sejong" alt="명예의 전당"/>
+                    <img src = {sejong} className="sejong" alt="명예의 전당"/>
                 </Link>
             </div>
             <div>
@@ -137,7 +125,6 @@ export default function Home() {
                 <img src={light} className="light5" alt="빛"/>
             </div>
             <div>
-                
                 {login === 0 &&
                     <button id="start" alt="시작" onClick={onStart}>
                         <div className="button-text">
@@ -166,10 +153,11 @@ export default function Home() {
                     <div>
                         <button className="enter-room">
                             <div className="button-text3">기존 방</div>
-                            <form onSubmit={onEnter}>
-                                <input type="text" name="code" placeholder="암호.." onChange={(e) => setCode(e.target.value)} className="enter"/>
-                                <input type="image" src={arrow} alt="들어가기" className="arrow"/>
-                            </form>
+                                <form onSubmit={onEnter}>
+                                    <input type="text" name="code" placeholder="암호.." onChange={(e) => setCode(e.target.value)} className="enter">
+                                    </input>
+                                    <input type="image" src={arrow} alt="들어가기" className="arrow"/>
+                                </form>
                         </button>
                         <button className="new-room">
                             <div className="button-text3">새로운 방</div>
